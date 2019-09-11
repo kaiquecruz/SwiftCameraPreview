92f260f165dbc50ec6604edb7b00d8d2d8d2af75
kaiquecruz <kaique--batista@hotmail.com>
2019-09-11 17:53:00 -0300

    Update SwiftCameraPreview.swift

--- src/ios/SwiftCameraPreview.swift
+++ src/ios/SwiftCameraPreview.swift
@@ -44,7 +44,7 @@ import AVFoundation
 		videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
 		videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
 		videoPreviewLayer?.frame = CGRect(x: x, y: y, width: width, height: height)
-        	videoPreviewLayer?.zPosition = z
+        	videoPreviewLayer?.zPosition = CGFloat(z)
 		self.webView.layer.addSublayer(videoPreviewLayer!)
 
 		capturePhotoOutput = AVCaptureStillImageOutput()
